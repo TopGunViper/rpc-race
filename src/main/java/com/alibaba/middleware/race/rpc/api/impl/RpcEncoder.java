514f4c658989ba46cf2ddb4dfbe430c86d6a3f6e
TopGunViper <1449978622@qq.com>
2016-03-10 22:52:39 +0800

    update

--- src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcEncoder.java
+++ src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcEncoder.java
@@ -1,5 +1,19 @@
 package com.alibaba.middleware.race.rpc.api.impl;
 
-public class RpcEncoder {
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
 
+public class RpcEncoder extends MessageToByteEncoder{
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out)
+			throws Exception {
+		// TODO Auto-generated method stub
+        byte[] body = SerializableUtil.FSTserialize(msg);
+        int dataLength = body.length;
+        out.writeInt(dataLength);
+        out.writeBytes(body);
+	}
+	
 }
