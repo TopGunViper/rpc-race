4a76827d080242147b619bc78c45efdf5de93174
TopGunViper <1449978622@qq.com>
2016-03-11 23:37:34 +0800

    实现对RpcRequest进行编码，Object->byte[]

--- src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcEncoder.java
+++ src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcEncoder.java
@@ -4,13 +4,19 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
-public class RpcEncoder extends MessageToByteEncoder{
+/**
+ * 
+ * 
+ * @author wqx
+ *
+ */
+public class RpcEncoder extends MessageToByteEncoder<Object>{
 
 	@Override
 	protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out)
 			throws Exception {
 		// TODO Auto-generated method stub
-        byte[] body = null;//SerializableUtil.FSTserialize(msg);
+        byte[] body = SerializableUtil.FSTserialize(msg);
         int dataLength = body.length;
         out.writeInt(dataLength);
         out.writeBytes(body);
