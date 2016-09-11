a69b49958b1775181e41520b689eafe537180d01
TopGunViper <1449978622@qq.com>
2016-09-11 22:43:01 +0800

    自定义异常类型RpcException

---
+++ src/main/java/edu/ouc/rpc/RpcException.java
@@ -0,0 +1,16 @@
+package edu.ouc.rpc;
+
+/**
+ * 自定义异常
+ * 
+ * @author wqx
+ *
+ */
+public class RpcException extends RuntimeException {
+
+	private static final long serialVersionUID = -2157872157006208360L;
+	
+	public RpcException(String msg){
+		super(msg);
+	}
+}
