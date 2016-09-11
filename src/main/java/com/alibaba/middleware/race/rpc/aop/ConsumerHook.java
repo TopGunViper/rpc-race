52d6a610b1578e5a3ea8d671402af662b115b737
TopGunViper <1449978622@qq.com>
2016-09-11 22:48:26 +0800

    delete deprecate class

--- src/main/java/com/alibaba/middleware/race/rpc/aop/ConsumerHook.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.middleware.race.rpc.aop;
-
-import com.alibaba.middleware.race.rpc.model.RpcRequest;
-
-public interface ConsumerHook {
-    public void before(RpcRequest request);
-    public void after(RpcRequest request);
-}
--- src/main/java/com/alibaba/middleware/race/rpc/async/ResponseCallbackListener.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.middleware.race.rpc.async;
-
-public interface ResponseCallbackListener {
-	
-	void onResponse(Object response);
-	void onTimeout();
-	void onException(Exception e);
-	
-}
